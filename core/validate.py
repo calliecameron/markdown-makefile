445f68e7d6850aa5631558a658469a90c07fea88
Callie Cameron <cjcameron7@gmail.com>
2022-12-12 13:25:17 +0000

    Cleanup metadata validation.

--- core/validate.py
+++ core/validate.py
@@ -5,13 +5,13 @@ import datetime
 import json
 import sys
 
-STR_KEYS = [
+PUBLICATION_STR_KEYS = [
     'venue',
     'paid',
     'note',
 ]
 
-DATE_KEYS = [
+PUBLICATION_DATE_KEYS = [
     'submitted',
     'rejected',
     'withdrawn',
@@ -19,14 +19,18 @@ DATE_KEYS = [
     'published',
 ]
 
-KEYS = frozenset(
-    STR_KEYS + DATE_KEYS + ['urls']
+PUBLICATION_KEYS = frozenset(
+    PUBLICATION_STR_KEYS + PUBLICATION_DATE_KEYS + ['urls']
 )
 
 
 def fail(msg: str) -> None:
     sys.stderr.write('ERROR: ' + msg)
     sys.exit(1)
+
+
+def fail_metadata(msg: str) -> None:
+    fail('invalid metadata: ' + msg)
 
 
 def validate_str(s: str) -> None:
@@ -63,55 +67,63 @@ def validate_text(j: Dict[str, Any]) -> None:
         walk_dict(j['meta']['title'])
 
 
+def assert_is_list(j: Dict[str, Any], msg: str) -> None:
+    if j['t'] != 'MetaList':
+        fail_metadata(msg)
+
+
+def assert_is_dict(j: Dict[str, Any], msg: str) -> None:
+    if j['t'] != 'MetaMap':
+        fail_metadata(msg)
+
+
+def assert_is_string(j: Dict[str, Any], msg: str) -> None:
+    if j['t'] != 'MetaInlines':
+        fail_metadata(msg)
+
+
 def validate_publications(j: Dict[str, Any]) -> None:
     if 'meta' not in j or 'publications' not in j['meta']:
         return
     ps = j['meta']['publications']
-    if ps['t'] != 'MetaList':
-        fail("invalid metadata: 'publications' must be a list")
+    assert_is_list(ps, "'publications' must be a list")
     for p in ps['c']:
-        if p['t'] != 'MetaMap':
-            fail("invalid metadata: item in 'publbications' must be a dict")
+        assert_is_dict(p, "item in 'publbications' must be a dict")
         data = p['c']
         keys = frozenset(data.keys())
-        if not keys.issubset(KEYS):
-            fail("invalid metadata: unknown keys %s in 'publications' item" % (keys - KEYS))
+        if not keys.issubset(PUBLICATION_KEYS):
+            fail_metadata("unknown keys %s in 'publications' item" % (keys - PUBLICATION_KEYS))
 
-        for k in sorted(STR_KEYS):
+        for k in sorted(PUBLICATION_STR_KEYS):
             if k in data:
-                if data[k]['t'] != 'MetaInlines':
-                    fail("invalid metadata: '%s' in 'publications' item must be a string" % k)
+                assert_is_string(data[k], "'%s' in 'publications' item must be a string" % k)
 
-        for k in sorted(DATE_KEYS):
+        for k in sorted(PUBLICATION_DATE_KEYS):
             if k in data:
                 if (data[k]['t'] != 'MetaInlines' or
                     len(data[k]['c']) != 1 or
                         data[k]['c'][0]['t'] != 'Str'):
-                    fail("invalid metadata: '%s' in 'publications' item must be a string" % k)
+                    fail_metadata("'%s' in 'publications' item must be a string" % k)
                 v = data[k]['c'][0]['c']
                 try:
                     datetime.date.fromisoformat(v)
                 except ValueError:
-                    fail(
-                        "invalid metadata: '%s' in 'publications' item must be a date; got '%s'" %
-                        (k, v))
+                    fail_metadata("'%s' in 'publications' item must be a date; got '%s'" % (k, v))
 
         if 'urls' in data:
             urls = data['urls']
-            if urls['t'] != 'MetaList':
-                fail("invalid metadata: 'urls' in 'publications' item must be a list")
+            assert_is_list(urls, "'urls' in 'publications' item must be a list")
             for url in urls['c']:
-                if url['t'] != 'MetaInlines':
-                    fail("invalid metadata: 'urls' item in 'publications' item must be a string")
+                assert_is_string(url, "'urls' item in 'publications' item must be a string")
 
         keys = frozenset(data.keys())
         if len(keys & frozenset(['accepted', 'rejected', 'withdrawn'])) > 1:
-            fail("invalid metadata: 'accepted', 'rejected' and 'withdrawn' in 'publications' item "
-                 "are mutually exclusive")
+            fail_metadata("'accepted', 'rejected' and 'withdrawn' in 'publications' item "
+                          "are mutually exclusive")
 
         if ('rejected' in keys or 'withdrawn' in keys) and 'published' in keys:
-            fail("invalid metadata: 'published' cannot be specified for publication items that "
-                 "were rejected or withdrawn")
+            fail_metadata("'published' cannot be specified for publication items that "
+                          "were rejected or withdrawn")
 
 
 def validate() -> None:

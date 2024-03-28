066c64cf79788f7d560270758dc7138ecdd7c56d
Callie Cameron <cjcameron7@gmail.com>
2024-03-28 16:18:01 +0100

    Update pymarkdownlnt.

--- markdown/core/lint/standard_lint.py
+++ markdown/core/lint/standard_lint.py
@@ -1,11 +1,14 @@
 import sys
 
-from pymarkdown.main import PyMarkdownLint  # type: ignore[import-untyped]
+from pymarkdown.main import PyMarkdownLint
 
 out_file = sys.argv[1]
 del sys.argv[1]
 
-PyMarkdownLint().main()
-
-with open(out_file, mode="w", encoding="utf-8") as f:
-    f.write("OK\n")
+try:
+    PyMarkdownLint().main()
+except SystemExit as e:
+    if not e.code:
+        with open(out_file, mode="w", encoding="utf-8") as f:
+            f.write("OK\n")
+    raise

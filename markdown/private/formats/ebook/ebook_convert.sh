58f086f778fcbb4e2d1a658100f4162b7e680d8e
Callie Cameron <cjcameron7@gmail.com>
2024-10-08 11:53:59 +0200

    Switch calibre to nix.

--- markdown/private/formats/ebook/ebook_convert.sh
+++ markdown/private/formats/ebook/ebook_convert.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 # ebook-convert is chatty, so we wrap it and capture the output.
 
-OUTPUT="$("${1}" "${@:2}" 2>&1)"
+set -u
+
+function usage() {
+    echo "Usage: $(basename "${0}") ebook_convert args..."
+    exit 1
+}
+
+test -z "${1:-}" && usage
+EBOOK_CONVERT="${1}"
+
+OUTPUT="$("${EBOOK_CONVERT}" "${@:2}" 2>&1)"
 EXIT_CODE="$?"
 
 if [ "${EXIT_CODE}" != '0' ]; then

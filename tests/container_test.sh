7da1a0197757432bfccae5fa9495c2816db75e40
Callie Cameron <cjcameron7@gmail.com>
2024-10-10 16:31:44 +0200

    Make the container get the nix commit from the module.

--- tests/container_test.sh
+++ tests/container_test.sh
@@ -6,9 +6,17 @@ set -eu
 THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "${THIS_DIR}"
 
+NIXPKGS_COMMIT="$(tr '\n' '\t' <../MODULE.bazel |
+    grep -o -P 'nix_repo\.github\(.*?\)' |
+    tr '\t' '\n' |
+    grep commit |
+    grep -o '".*"' |
+    sed 's/"//g')"
+
 podman build \
     -f Containerfile \
     --build-arg "uid=$(id -u)" \
     --build-arg "gid=$(id -g)" \
+    --build-arg "nixpkgs_commit=${NIXPKGS_COMMIT}" \
     -t rules-markdown-container-test \
     ../

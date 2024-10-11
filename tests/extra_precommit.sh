41c5c3853aee42cd8b887106d600fc110b3d8812
Callie Cameron <cjcameron7@gmail.com>
2024-10-11 17:05:21 +0200

    Add a module version consistency test.

--- tests/extra_precommit.sh
+++ tests/extra_precommit.sh
@@ -7,4 +7,22 @@ if ! grep "$(date '+%Y')" 'LICENSE' &>/dev/null; then
     exit 1
 fi
 
+MODULE_VERSION="$(tr '\n' '\t' <MODULE.bazel |
+    grep -o -E 'module\([^\)]+version = "([0-9]|\.)+"[^\)]+\)' |
+    tr '\t' '\n' |
+    grep 'version' |
+    grep -o '".*"' |
+    sed 's/"//g')"
+USED_VERSION="$(tr '\n' '\t' <tests/other_workspace/MODULE.bazel |
+    grep -o -E 'bazel_dep\([^\)]+name = "rules_markdown"[^\)]+version = "([0-9]|\.)+"[^\)]+\)' |
+    tr '\t' '\n' |
+    grep 'version' |
+    grep -o '".*"' |
+    sed 's/"//g')"
+
+if [ "${USED_VERSION}" != "${MODULE_VERSION}" ]; then
+    echo "Error: rules_markdown version used in tests/other_workspace/MODULE.bazel doesn't match main MODULE.bazel"
+    exit 1
+fi
+
 exit 0
